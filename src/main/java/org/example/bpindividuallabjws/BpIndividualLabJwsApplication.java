07867ae57baf111d3fe9453f4a56c80109a04141
BP <valkarian@valkarian.com>
2025-08-28 11:11:46 +0200

    Initial commit

---
+++ src/main/java/org/example/bpindividuallabjws/BpIndividualLabJwsApplication.java
@@ -0,0 +1,13 @@
+package org.example.bpindividuallabjws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpIndividualLabJwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BpIndividualLabJwsApplication.class, args);
+    }
+
+}
